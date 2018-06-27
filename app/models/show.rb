9e12935750fd2eb8126843d1be6df99ce0adf650
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-27 00:43:01 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,7 @@
+require 'pry'
 class Show < ActiveRecord::Base
   
-  def highest_rating
-    shows.maximum("rating")
-  end
-  
-  def most_popular_show
-    show.highest_rating
-  end
-  
+  def self.highest_rating
+    shows.maximum(rating)
+
 end
