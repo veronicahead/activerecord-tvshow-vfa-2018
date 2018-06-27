c96651940566b76b95d30ce4866f01a4756b3189
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-27 00:46:28 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -22,7 +22,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.popular_shows
-    self.where(:rating > ?, 5)
+    self.where(":rating > ?", 5)
   end
   
   def self.shows_by_alphabetical_order
