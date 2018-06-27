c1dbe16129655ee3f85c6e7ba01e98aa21df20f9
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-27 00:44:17 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,5 +4,19 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+  
+  def self.most_popular_show
+    self.highest_rating
+  end
+  
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+  
+  def self.least_popular_show
+    self.lowest_rating
+  end
+  
+  
 
 end
